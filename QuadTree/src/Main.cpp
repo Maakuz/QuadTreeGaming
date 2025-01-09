4850d66287c52797e66a25c12e4320a4c85d14b8
Markus Werdien <markuswerdien@hotmail.com>
2025-01-09 21:42:20 +0100

    Made it into a template and added element insertion.
    The drawing of the tree is horrible and needs to be improved for debugging purposes.

--- QuadTree/src/Main.cpp
+++ QuadTree/src/Main.cpp
@@ -1,24 +1,62 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Mouse.hpp>
 #include "QuadTree.h"
-int main() {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    QuadTree tree(sf::Vector2f(100,100), 200, 0);
 
+void drawNode(QuadTree<int>::QuadTreeNode* node, sf::RenderWindow* wind, int depth = 0);
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 2000, 2000 }), "SFML works!");
+    window.setFramerateLimit(120);
+    sf::CircleShape shape(10.f);
+    shape.setOrigin(sf::Vector2f(10, 10));
+    shape.setPosition(sf::Vector2f(570, 570));
+    shape.setFillColor(sf::Color::Blue);
+
+    QuadTree<int> tree(sf::Vector2f(0, 0), 2000, 7);
+    sf::Clock clock;
 
     while (window.isOpen())
     {
+        float currentTime = clock.restart().asMilliseconds();
+        printf("%f\n", currentTime);
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        {
+            // left mouse button is pressed: shoot
+            shape.setPosition((sf::Vector2f)sf::Mouse::getPosition(window));
+            tree.insert(1, shape.getPosition());
+        }
+
+        static bool drawTree = true;
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right))
+            drawTree = !drawTree;
+
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-
         window.clear();
         window.draw(shape);
+        auto root = tree.getRoot();
+        if (drawTree)
+            drawNode(&root, &window);
         window.display();
     }
 
     return 0;
+}
+
+void drawNode(QuadTree<int>::QuadTreeNode* node, sf::RenderWindow* wind, int depth) { 
+    if (!node->isLeaf()) {
+        for (auto n : *node->getNodesPtr()) {
+            drawNode(&n, wind, depth + 1);
+        }
+    }
+    sf::RectangleShape rect(sf::Vector2f(1, 1) * node->getSize());
+    rect.setFillColor(sf::Color::Transparent);
+    rect.setOutlineColor(sf::Color(0, 255, 0));
+    rect.setPosition(node->getPos());
+    rect.setOutlineThickness(1);
+    wind->draw(rect);
 }
--- QuadTree/src/QuadTree.h
+++ QuadTree/src/QuadTree.h
@@ -1,31 +1,11 @@
 #pragma once
 #include <SFML/Graphics/Transform.hpp>
 #include <vector>
-class QuadTreeNode {
-private:
-    sf::Vector2f m_pos;
-    float m_size;
-    std::vector<QuadTreeNode> m_nodes;
 
-public:
-    QuadTreeNode() {}
-    QuadTreeNode(sf::Vector2f pos, float size);
-    void subdivide(int depth);
-    std::vector<QuadTreeNode> getNodes() { return m_nodes; }
-    sf::Vector2f getPos() { return m_pos; }
-    float getSize() { return m_size; }
-    
-
-};
-
+template <typename T>
 class QuadTree {
-
-private:
-    QuadTreeNode m_node;
-
-    int m_depth;
-
 public:
+
     enum index {
         topLeft = 0,    //00
         topRight = 2,   //10
@@ -33,7 +13,98 @@ public:
         bottomRight = 3 //11
     };
 
+    class QuadTreeNode {
+    private:
+        sf::Vector2f m_pos;
+        float m_size;
+        std::vector<QuadTreeNode> m_nodes;
+        T value;
+
+    public:
+        QuadTreeNode() {}
+        QuadTreeNode(sf::Vector2f pos, float size);
+        void subdivide(sf::Vector2f targetPos, T val, int depth = 0);
+        std::vector<QuadTreeNode>* getNodesPtr() { return &m_nodes; }
+        sf::Vector2f getPos() { return m_pos; }
+        float getSize() { return m_size; }
+        bool isLeaf() { return m_nodes.size() == 0; }
+
+
+    };
+
+private:
+    QuadTreeNode m_node;
+    int m_depth;
+
+    static int getIndexOfPosition(sf::Vector2f lookupPos, sf::Vector2f nodePos);
+
+public:
     QuadTree(sf::Vector2f pos, float size, int depth);
+
+    QuadTreeNode getRoot() { return m_node; }
+    void insert(T val, sf::Vector2f pos);
 
 };
 
+template <typename T>
+QuadTree<T>::QuadTreeNode::QuadTreeNode(sf::Vector2f pos, float size) {
+    m_pos = pos;
+    m_size = size;
+};
+
+template <typename T>
+void QuadTree<T>::QuadTreeNode::subdivide(sf::Vector2f targetPos, T val, int depth) {
+    int index = getIndexOfPosition(targetPos, sf::Vector2f(m_pos.x + (m_size * 0.5f), m_pos.y + (m_size * 0.5f)));
+    
+    //if it exist just move on to the next layer
+    if (!isLeaf()) {
+        if (depth > 0)
+            m_nodes[index].subdivide(targetPos, val, depth - 1);
+        return;
+    }
+
+    m_nodes.resize(4);
+
+    for (int i = 0; i < m_nodes.size(); i++) {
+        sf::Vector2f newPos = m_pos;
+
+        if ((i & 2) == 2) {
+            newPos.x += m_size * 0.5f;
+        }
+
+        if ((i & 1) == 1) {
+            newPos.y += m_size * 0.5f;
+        }
+
+        m_nodes[i] = QuadTreeNode(newPos, m_size * 0.5f);
+        if (depth > 0 && index == i)
+            m_nodes[i].subdivide(targetPos, val, depth - 1);
+    }
+}
+
+
+
+
+//SUBCLASS ENDS HERE
+
+template <typename T>
+QuadTree<T>::QuadTree(sf::Vector2f pos, float size, int depth) :
+    m_node(pos, size) {
+    m_depth = depth;
+    //m_node.subdivide(depth);
+};
+
+template <typename T>
+int QuadTree<T>::getIndexOfPosition(sf::Vector2f lookupPos, sf::Vector2f nodePos) {
+    int index = 0;
+
+    index = index | (lookupPos.x > nodePos.x ? 2 : 0);
+    index = index | (lookupPos.y > nodePos.y ? 1 : 0);
+
+    return index;
+};
+
+template<typename T>
+inline void QuadTree<T>::insert(T val, sf::Vector2f pos) {
+    m_node.subdivide(pos, val, m_depth);
+};
